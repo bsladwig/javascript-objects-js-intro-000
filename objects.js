48dc1154cc0c69d7c8da6e883b7a1f03f95808e9
Bonnie Ladwig <bsladwig@gmail.com>
2020-05-12 16:49:34 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,7 +6,7 @@ function updatePlaylist(playlist, artistName, song) {
   return playlist
 }
 
-function removeFromPlaylist(playlist, artistName, song) {
+function removeFromPlaylist(playlist, artistName) {
   delete playlist [artistName];
   return playlist;
 }
